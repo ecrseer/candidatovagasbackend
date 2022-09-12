3bd291a2aa4c61295775f06e3cd2024bd229ca64
ecrseer <juustino@live.com>
2022-09-12 20:26:01 -0300

    first commit

---
+++ src/main/java/br/infnet/edu/gabrielj/backendcandidato/backendcandidato/BackendcandidatoApplication.java
@@ -0,0 +1,13 @@
+package br.infnet.edu.gabrielj.backendcandidato.backendcandidato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendcandidatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendcandidatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/infnet/edu/gabrielj/backendcandidato/backendcandidato/BackendcandidatoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.infnet.edu.gabrielj.backendcandidato.backendcandidato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendcandidatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
